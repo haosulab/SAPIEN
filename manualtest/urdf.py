d6baa85bef489335a05f5ca860fd2944deb38054
Fanbo Xiang <xiangfanbo@gmail.com>
2020-01-17 22:07:45 -0800

    py urdf example

---
+++ manualtest/urdf.py
@@ -0,0 +1,34 @@
+import pysapien
+import numpy as np
+from pysapien import Pose
+from transforms3d.quaternions import axangle2quat as aa
+
+
+sim = pysapien.Simulation()
+renderer = pysapien.OptifuserRenderer()
+sim.set_renderer(renderer)
+render_controller = pysapien.OptifuserController(renderer)
+
+render_controller.show_window()
+
+s0 = sim.create_scene()
+s0.add_ground(-1)
+s0.set_timestep(1 / 240)
+
+s0.set_ambient_light([0.5, 0.5, 0.5])
+s0.set_shadow_light([0, 1, -1], [0.5, 0.5, 0.5])
+
+loader = s0.create_urdf_loader()
+loader.fix_base = 0
+chair = loader.load("../assets/179/mobility.urdf")
+
+render_controller.camera.set_position([-5, 0, 0])
+render_controller.set_current_scene(s0)
+
+while not render_controller.should_quit:
+    s0.update_render()
+    for i in range(4):
+        s0.step()
+    render_controller.render()
+
+s0 = None
