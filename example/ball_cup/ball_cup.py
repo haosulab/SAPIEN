c6020eb69e5ea04c83141e3df0bf47a121ce851e
Fanbo Xiang <xiangfanbo@gmail.com>
2020-02-04 14:32:53 -0800

    improve cup example

--- example/ball_cup/ball_cup.py
+++ example/ball_cup/ball_cup.py
@@ -10,7 +10,7 @@ render_control = sapien.OptifuserController(renderer)  # used to control the ren
 # create simulcation and scene
 sim = sapien.Simulation()
 sim.set_renderer(renderer)
-scene = sim.create_scene()
+scene: sapien.Scene = sim.create_scene()
 scene.set_timestep(1 / 60)
 
 # create scene
@@ -21,11 +21,14 @@ scene.set_shadow_light([1, -1, -1], [.5, .5, .5])
 # load cup
 cup_file_name = "cup.obj"
 cup_pose = Pose([0, 0, 0])
-cup_scale = [20] * 3
+cup_scale = [7] * 3
 cup_builder = scene.create_actor_builder()
 cup_builder.add_multiple_convex_shapes_from_file(cup_file_name, cup_pose, cup_scale)
 cup_builder.add_visual_from_file(cup_file_name, cup_pose, cup_scale)
-cup = cup_builder.build()
+cup1 = cup_builder.build()
+
+cup2 = cup_builder.build()
+cup2.set_pose(Pose([2, 0, 0]))
 
 
 # make spheres
@@ -34,22 +37,21 @@ def build_sphere(pos, r):
     # add physics for sphere
     sphere_builder.add_sphere_shape(Pose(pos), r, None, 400)  # 400 is the density
     # add visual display for sphere
-    sphere_builder.add_sphere_visual(Pose(pos), r, color=[1, 1, 1])
+    sphere_builder.add_sphere_visual(Pose(pos), r, color=[0, 1, 1])
     return sphere_builder.build()
 
 
-build_sphere([0,0,2], 0.02)
-# num_balls = 10
-# ball_r = 0.1
-# balls_distr_r = 0.03
-# for i in range(num_balls):
-#     pos_z = np.random.uniform(1, 2)
-#     rand_theta = np.random.rand() * np.pi * 2
-#     pos_x = np.cos(rand_theta) * balls_distr_r
-#     pos_y = np.cos(rand_theta) * balls_distr_r
-#     pos = [pos_x, pos_y, pos_z]
-#     build_sphere(pos, ball_r)
-
+num_balls = 50
+ball_r = 0.1
+balls_distr_r = 0.03
+for i in range(num_balls):
+    pos_z = np.random.uniform(1, 1)
+    rand_theta = np.random.rand() * np.pi * 2
+    pos_x = np.cos(rand_theta) * balls_distr_r
+    pos_y = np.cos(rand_theta) * balls_distr_r
+    pos = [pos_x, pos_y, pos_z + i * 0.2]
+    b = build_sphere(pos, ball_r)
+    b.set_name('ball' + str(i))
 
 # render
 render_control.set_current_scene(scene)
@@ -57,11 +59,40 @@ render_control.set_camera_position(-10, -10, 5)
 render_control.set_camera_rotation(np.pi / 4, -np.pi / 9)
 render_control.show_window()
 
+count = 0
 # simulation loop
 while not render_control.should_quit:
+    ground_ball = set()
+    count += 1
+    if 120 < count < 200:
+        cup1.add_force_torque([90, 0, 3500], [0, 0, 20])
+    if 200 < count < 240:
+        cup1.add_force_torque([0, 0, 3500], [0, 0, -20])
+    if 240 < count < 360:
+        cup1.add_force_torque([20, 0, 2800], [0, 100, 0])
+    if 360 < count < 480:
+        cup1.add_force_torque([0, 0, 2800], [0, 0, 0])
+
+    contacts = scene.get_contacts()
+    for c in contacts:
+        if c.actor1.name == 'ground':
+            a = c.actor2
+        elif c.actor2.name == 'ground':
+            a = c.actor1
+        else:
+            continue
+        if a.name.startswith('ball') and c.separation < 1e-6:
+            ground_ball.add(a)
+
+    if count % 60 == 0:
+        print(len(ground_ball), 'balls are on the ground!')
+
     # simulate physics
     scene.step()
     # update render
     scene.update_render()
     # render the frame
     render_control.render()
+
+
+scene = 0
