c9ea3486510210920629c726026ddf02b3e6ccca
Fanbo Xiang <xiangfanbo@gmail.com>
2020-03-02 19:48:47 -0800

    better access token

--- python/py_package/asset/__init__.py
+++ python/py_package/asset/__init__.py
@@ -6,7 +6,12 @@ import os
 url = 'https://sapien.ucsd.edu/api/download/compressed/{}.zip?token={}'
 
 
-def download_partnet_mobility(model_id, token, directory=None):
+def download_partnet_mobility(model_id, token=None, directory=None):
+    if token is None:
+        if 'SAPIEN_ACCESS_TOKEN' not in os.environ:
+            raise Exception('To download the model, you need to provide the token or set the SAPIEN_ACCESS_TOKEN environment variable.')
+        token = os.environ['SAPIEN_ACCESS_TOKEN']
+
     if not directory:
         directory = 'partnet-mobility-dataset'
     urdf_file = os.path.join(directory, str(model_id), 'mobility.urdf')
@@ -16,6 +21,7 @@ def download_partnet_mobility(model_id, token, directory=None):
         return urdf_file
 
     # download file
+    print('downloading from', url.format(model_id, token))
     r = requests.get(url.format(model_id, token), stream=True)
     if not r.ok:
         raise Exception("Download PartNet-Mobility failed")
