97ed9bd1f3d69990897e681ff30fea6fdfba4e82
Fanbo Xiang <xiangfanbo@gmail.com>
2020-03-15 22:47:50 -0700

    bug fixes

--- example/sapien_core_example/mocap.py
+++ example/sapien_core_example/mocap.py
@@ -11,56 +11,40 @@ render_controller = sapien.OptifuserController(renderer)
 render_controller.show_window()
 
 scene: sapien.Scene = sim.create_scene()
-scene.add_ground(0)
-scene.set_timestep(1 / 60)
+scene.set_timestep(1 / 500)
 
 scene.set_ambient_light([0.5, 0.5, 0.5])
 scene.set_shadow_light([0, 1, -1], [0.5, 0.5, 0.5])
 
 loader = scene.create_urdf_loader()
 loader.fix_root_link = 1
-robot = loader.load("../assets_local/robot/all_robot.urdf")
+robot = loader.load("assets/robot/jaco2.urdf")
 
 actor_builder: sapien.ActorBuilder = scene.create_actor_builder()
 actor_builder.add_sphere_visual(Pose(), 0.02, [1, 0, 0], "anchor")
-anchor: sapien.Actor = actor_builder.build_static()
-anchor.set_pose(Pose([1, 0, 0.5]))
+anchor = actor_builder.build_static()
+anchor.set_pose(Pose([0.2, 0, 0.2]))
 
 render_controller.set_camera_position(-0.5, 2, 0.5)
 render_controller.set_camera_rotation(-1, 0)
 render_controller.set_current_scene(scene)
 
-gripper = [l for l in robot.get_links() if l.name == 'right_gripper_base_link'][0]
-
+gripper = [l for l in robot.get_links() if l.name == 'j2s7s300_end_effector'][0]
 
 def move_anchor(dx, dy, dz, droll, dpitch, dyaw):
     quat = transforms3d.euler.euler2quat(droll, dpitch, dyaw)
     anchor.set_pose(Pose([dx, dy, dz]) * anchor.pose * Pose([0, 0, 0], quat))
 
 
-def open_gripper():
-    f = np.zeros(13)
-    f[8:11] = [-0.01]
-    robot.set_qf(f)
-
-
-def close_gripper():
-    f = np.zeros(13)
-    f[8:11] = [0.01]
-    robot.set_qf(f)
-
-
-tilt = [j for j in robot.get_joints() if j.name == 'tilt_joint'][0]
-tilt.set_drive_property(100, 200)
-tilt.set_drive_target(0)
-
-pan = [j for j in robot.get_joints() if j.name == 'pan_joint'][0]
-pan.set_drive_property(100, 200)
-pan.set_drive_target(0)
-
 drive = scene.create_drive(anchor, Pose(), gripper, Pose())
 drive.set_properties(4000, 8000)
 drive.set_target(Pose())
+
+arm_init_qpos = [4.71, 2.84, 0, 0.75, 4.62, 4.48, 4.88]
+gripper_init_qpos = [0.] * 6
+init_qpos = arm_init_qpos + gripper_init_qpos
+robot.set_qpos(init_qpos)
+robot.set_qvel([0] * 13)
 
 while not render_controller.should_quit:
     if render_controller.input.get_key_state(ord('I')):
@@ -93,10 +77,6 @@ while not render_controller.should_quit:
             move_anchor(0, 0, 0, 0, 0, -0.05)
         else:
             move_anchor(0, 0, -0.05, 0, 0, 0)
-    if render_controller.input.get_key_state(ord('G')):
-        open_gripper()
-    if render_controller.input.get_key_state(ord('H')):
-        close_gripper()
 
     scene.update_render()
     for i in range(1):
