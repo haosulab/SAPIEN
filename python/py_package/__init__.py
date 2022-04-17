78bd5f5c5455f319254f2aca06d4422a6c58e0de
Fanbo Xiang <xiangfanbo@gmail.com>
2022-04-17 16:50:01 -0700

    warn about wsl

--- python/py_package/__init__.py
+++ python/py_package/__init__.py
@@ -1,2 +1,10 @@
+import os
+import warnings
+if not os.path.exists("/proc/version"):
+    warnings.warn("You are not using a standard Linux based OS. SAPIEN probably will not work")
+with open("/proc/version") as f:
+    if 'microsoft' in '\n'.join(f.readlines()).lower():
+        warnings.warn("It seems you are using WSL. SAPIEN renderer is not supported on WSL")
+
 from sapien import core, sensor, asset, example, utils
 from sapien.version import __version__
