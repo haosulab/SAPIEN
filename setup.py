ecc5033a699eece3f64401e75cb89f17172fefb6
Fanbo Xiang <xiangfanbo@gmail.com>
2022-10-25 22:26:15 -0700

    do not provide icd

--- setup.py
+++ setup.py
@@ -75,13 +75,6 @@ class CMakeBuild(build_ext):
         assert os.path.exists(source_path)
         shutil.copytree(source_path, vulkan_shader_path)
 
-        vulkan_icd_path = os.path.join(self.build_lib, 'sapien', 'vulkan_icd')
-        source_path = os.path.join(ext.sourcedir, 'vulkan_icd')
-        if os.path.exists(vulkan_icd_path):
-            shutil.rmtree(vulkan_icd_path)
-        assert os.path.exists(source_path)
-        shutil.copytree(source_path, vulkan_icd_path)
-
         kuafu_shader_path = os.path.join(self.build_lib, 'sapien', 'kuafu_assets', 'shaders')
         source_path = os.path.join(ext.sourcedir, '3rd_party/kuafu/resources/shaders')
         if os.path.exists(kuafu_shader_path):
