0a339fa859299029fd8a609a7636c8224aca3792
Ang Li <ang6li98@gmail.com>
2023-01-21 15:13:45 -0800

    add support for dlpack in simsense

--- python/py_package/sensor/depth_processor.py
+++ python/py_package/sensor/depth_processor.py
@@ -339,17 +339,22 @@ def calc_main_depth_from_left_right_ir(
 
     return depth
 
+
 # GPU Depth Sensor
 class DepthSensorCUDA:
-    def __init__(self, lr_size, k_l, k_r, l2r, rgb_size=None, k_rgb=None, l2rgb=None, min_depth=0.0, max_depth=10.0, rectified=False,
-                    census_width=9, census_height=7, max_disp=128, block_width=1, block_height=1, p1_penalty=7, p2_penalty=86,
-                    uniqueness_ratio=0, lr_max_diff=1, median_filter_size=3, depth_dilation=False):
+    def __init__(
+        self, lr_size, k_l, k_r, l2r, rgb_size=None, k_rgb=None, l2rgb=None, min_depth=0.0, max_depth=10.0,
+        ir_noise_seed=0, speckle_shape=0.0, speckle_scale=0.0, gaussian_mu=0.0, gaussian_sigma=0.0, rectified=False,
+        census_width=7, census_height=7, max_disp=128, block_width=7, block_height=7, p1_penalty=7, p2_penalty=86,
+        uniqueness_ratio=15, lr_max_diff=1, median_filter_size=3, depth_dilation=False
+    ):
+        from ..core.pysapien.simsense import DepthSensorEngine
         """
-        Initiate the DepthSensor class. The camera frame follows the OpenCV coordinate system, which is x right, y down, z forward.
-        In this sense, the origin of the right camera's frame should have positive x value when viewed in left camera's frame. Left,
-        right and RGB image are assumed to be undistorted. By default, the final depth map will be presented in left camera's frame
-        with lr_size. Specifying rgb_size, k_rgb and l2rgb will turn on the registration mode, which will tranform the final depth
-        map from left camera's frame to specified RGB camera's frame with rgb_size.
+        Initiate the DepthSensor class. The camera frame follows the OpenCV coordinate system (x right, y down, z forward). Left,
+        right and RGB camera are assumed to be undistorted. By default, the final depth map will be presented in left camera's frame
+        with lr_size. Specifying rgb_size, k_rgb and l2rgb will turn on depth registration, which will tranform the final depth map
+        from left camera's frame to specified RGB camera's frame with rgb_size. Specifying speckle_shape > 0 will turn on infrared
+        noise simulation.
 
         :param lr_size: (width, height) of the left and right image.
         :param k_l: Intrinsic matrix of the left camera (in OpenCV coordinate system).
@@ -360,25 +365,27 @@ class DepthSensorCUDA:
         :param l2rgb: Change-of-coordinate matrix from left camera's frame to RGB camera's frame (in OpenCV coordinate system).
         :param min_depth: Minimum valid depth in meters.
         :param max_depth: Maximum valid depth (non-inclusive) in meters.
+        :param ir_noise_seed: Random seed for simulating infrared noise.
+        :param speckle_shape: Shape parameter for simulating infrared speckle noise (Gamma distribution). Set to 0 to disable
+                              noise simulation.
+        :param speckle_scale: Scale parameter for simulating infrared speckle noise (Gamma distribution).
+        :param gaussian_mu: Mean for simulating infrared thermal noise (Gaussian distribution).
+        :param gaussian_sigma: Standard deviation for simulating infrared thermal noise (Gaussian distribution).
         :param rectified: Whether the input has already been rectified. Set to true if no rectification is needed.
         :param census_width: Width of the center-symmetric census transform window. This must be an odd number.
         :param census_height: Height of the center-symmetric census transform window. This must be an odd number.
         :param max_disp: Maximum disparity search space (non-inclusive) for stereo matching.
         :param block_width: Width of the matched block. This must be an odd number.
         :param block_height: Height of the matched block. This must be an odd number.
-        :param p1_penalty: P1 penalty for semi-global matching algorithm. It is the penalty on the disparity change by plus or minus
-                           1 between neighboring pixels.
-        :param p2_penalty: P2 penalty for semi-global matching algorithm. It is the penalty on the disparity change by more than 1
-                           between neighboring pixels.
+        :param p1_penalty: P1 penalty for semi-global matching algorithm.
+        :param p2_penalty: P2 penalty for semi-global matching algorithm.
         :param uniqueness_ratio: Margin in percentage by which the minimum computed cost should win the second best (not considering
                                  best match's adjacent pixels) cost to consider the found match valid.
-        :param lr_max_diff: Maximum allowed difference in the left-right consistency check. Set it to 255 will disable the check.
-        :param median_filter_size: Size of the median filter. Choices are 1, 3, 5, 7. When set to 1 the median filter is turned off.
-        :param depth_dilation: Whether to dilate the final depth map to avoid holes. This is only effective when registration mode is on.
-                               Depth dilation is recommended when rgb_size is greater than lr_size.
+        :param lr_max_diff: Maximum allowed difference in the left-right consistency check. Set it to 255 to disable the check.
+        :param median_filter_size: Size of the median filter. Choices are 1, 3, 5, 7. Set to 1 to disable median filter.
+        :param depth_dilation: Dilate the final depth map to avoid holes when depth registration is on. Recommended when rgb_size
+                               is greater than lr_size.
         """
-        from ..core.pysapien.simsense import DepthSensorEngine
-
         img_w, img_h = lr_size
         registration = False
         if rgb_size != None or k_rgb != None or l2rgb != None:
@@ -389,17 +396,20 @@ class DepthSensorCUDA:
             raise TypeError("Image height and width must be integer no less than 32")
         
         if registration == True and (rgb_size is None or k_rgb is None or l2rgb is None):
-            raise TypeError("Registration mode is on but missing one or two RGB camera's parameters")
+            raise TypeError("Depth registration is on but missing one or two RGB camera's parameters")
 
+        if speckle_shape > 0 and (speckle_scale <= 0 or gaussian_sigma <= 0):
+            raise TypeError("Infrared noise simulation is on. Speckle_scale and gaussian_sigma must both be positive")
+        
         if not isinstance(census_width, int) or not isinstance(census_height, int) or census_width <= 0 or census_height <= 0 or \
-                census_width % 2 == 0 or census_height % 2 == 0 or census_width*census_height > 65:
+                census_width % 2 == 0 or census_height % 2 == 0 or census_width * census_height > 65:
             raise TypeError("census_width and census_height must be positive odd integers and their product should be no larger than 65")
         
         if not isinstance(max_disp, int) or max_disp < 32 or max_disp > 1024:
             raise TypeError("max_disp must be integer within range [32, 1024]")
         
         if not isinstance(block_width, int) or not isinstance(block_height, int) or block_width <= 0 or block_height <= 0 or \
-                block_width % 2 == 0 or block_height % 2 == 0 or block_width*block_height > 256:
+                block_width % 2 == 0 or block_height % 2 == 0 or block_width * block_height > 256:
             raise TypeError("block_width and block_height must be positive odd integers and their product should be no larger than 256")
  
         if not isinstance(p1_penalty, int) or not isinstance(p2_penalty, int) or p1_penalty <= 0 or p2_penalty <= 0 or \
@@ -430,15 +440,16 @@ class DepthSensorCUDA:
         if registration:
             # Get registration matrix
             a1, a2, a3, b = self._get_registration_mat(lr_size, k_l, k_rgb, l2rgb)
-            self.engine = DepthSensorEngine(img_h, img_w, rgb_size[1], rgb_size[0], f_len, b_len, min_depth, max_depth, rectified,
-                                            census_width, census_height, max_disp, block_width, block_height,
+            self.engine = DepthSensorEngine(img_h, img_w, rgb_size[1], rgb_size[0], f_len, b_len, min_depth, max_depth,
+                                            ir_noise_seed, speckle_shape, speckle_scale, gaussian_mu, gaussian_sigma,
+                                            rectified, census_width, census_height, max_disp, block_width, block_height,
                                             p1_penalty, p2_penalty, uniqueness_ratio, lr_max_diff, median_filter_size,
                                             map_lx, map_ly, map_rx, map_ry, a1, a2, a3, b[0], b[1], b[2], depth_dilation)
         else:
-            self.engine = DepthSensorEngine(img_h, img_w, f_len, b_len, min_depth, max_depth, rectified,
-                                            census_width, census_height, max_disp, block_width, block_height,
-                                            p1_penalty, p2_penalty, uniqueness_ratio, lr_max_diff, median_filter_size,
-                                            map_lx, map_ly, map_rx, map_ry)
+            self.engine = DepthSensorEngine(img_h, img_w, f_len, b_len, min_depth, max_depth, ir_noise_seed, speckle_shape,
+                                            speckle_scale, gaussian_mu, gaussian_sigma, rectified, census_width, census_height,
+                                            max_disp, block_width, block_height, p1_penalty, p2_penalty, uniqueness_ratio,
+                                            lr_max_diff, median_filter_size, map_lx, map_ly, map_rx, map_ry)
 
     def compute(self, img_l, img_r):
         """
@@ -451,34 +462,65 @@ class DepthSensorCUDA:
         result = self.engine.compute(img_l, img_r)
 
         return result
-    
-    def set_penalties(self, p1_penalty, p2_penalty):
-        if not isinstance(p1_penalty, int) or not isinstance(p2_penalty, int) or p1_penalty <= 0 or p2_penalty <= 0 or \
-                p1_penalty >= p2_penalty or p2_penalty >= 224:
-            raise TypeError("p1 must be positive integer less than p2 and p2 be positive integer less than 224")
-        self.engine._set_penalties(p1_penalty, p2_penalty)
+
+    def set_ir_noise_parameters(self, speckle_shape, speckle_scale, gaussian_mu, gaussian_sigma):
+        """
+        :param speckle_shape: Shape parameter for simulating infrared speckle noise (Gamma distribution). Set to 0 to disable
+                              noise simulation.
+        :param speckle_scale: Scale parameter for simulating infrared speckle noise (Gamma distribution).
+        :param gaussian_mu: Mean for simulating infrared thermal noise (Gaussian distribution).
+        :param gaussian_sigma: Standard deviation for simulating infrared thermal noise (Gaussian distribution).
+        """
+        if speckle_shape > 0 and (speckle_scale <= 0 or gaussian_sigma <= 0):
+            raise TypeError("Infrared noise simulation is on. Speckle_scale and gaussian_sigma must both be positive")
+        self.engine.set_ir_noise_parameters(speckle_shape, speckle_scale, gaussian_mu, gaussian_sigma)
     
     def set_census_window_size(self, census_width, census_height):
+        """
+        :param census_width: Width of the center-symmetric census transform window. This must be an odd number.
+        :param census_height: Height of the center-symmetric census transform window. This must be an odd number.
+        """
         if not isinstance(census_width, int) or not isinstance(census_height, int) or census_width <= 0 or census_height <= 0 or \
                 census_width % 2 == 0 or census_height % 2 == 0 or census_width*census_height > 65:
             raise TypeError("census_width and census_height must be positive odd integers and their product should be no larger than 65")
-        self.engine._set_census_window_size(census_width, census_height)
+        self.engine.set_census_window_size(census_width, census_height)
     
     def set_matching_block_size(self, block_width, block_height):
+        """
+        :param block_width: Width of the matched block. This must be an odd number.
+        :param block_height: Height of the matched block. This must be an odd number.
+        """
         if not isinstance(block_width, int) or not isinstance(block_height, int) or block_width <= 0 or block_height <= 0 or \
                 block_width % 2 == 0 or block_height % 2 == 0 or block_width*block_height > 256:
             raise TypeError("block_width and block_height must be positive odd integers and their product should be no larger than 256")
-        self.engine._set_matching_block_size(block_width, block_height)
+        self.engine.set_matching_block_size(block_width, block_height)
+    
+    def set_penalties(self, p1_penalty, p2_penalty):
+        """
+        :param p1_penalty: P1 penalty for semi-global matching algorithm.
+        :param p2_penalty: P2 penalty for semi-global matching algorithm.
+        """
+        if not isinstance(p1_penalty, int) or not isinstance(p2_penalty, int) or p1_penalty <= 0 or p2_penalty <= 0 or \
+                p1_penalty >= p2_penalty or p2_penalty >= 224:
+            raise TypeError("p1 must be positive integer less than p2 and p2 be positive integer less than 224")
+        self.engine.set_penalties(p1_penalty, p2_penalty)
     
     def set_uniqueness_ratio(self, uniqueness_ratio):
+        """
+        :param uniqueness_ratio: Margin in percentage by which the minimum computed cost should win the second best (not considering
+                                 best match's adjacent pixels) cost to consider the found match valid.
+        """
         if not isinstance(uniqueness_ratio, int) or uniqueness_ratio < 0 or uniqueness_ratio > 255:
             raise TypeError("uniqueness_ratio must be positive integer no larger than 255")
-        self.engine._set_uniqueness_ratio(uniqueness_ratio)
+        self.engine.set_uniqueness_ratio(uniqueness_ratio)
 
     def set_lr_max_diff(self, lr_max_diff):
+        """
+        :param lr_max_diff: Maximum allowed difference in the left-right consistency check. Set it to 255 to disable the check.
+        """
         if not isinstance(lr_max_diff, int) or lr_max_diff < -1 or lr_max_diff > 255:
             raise TypeError("lr_max_diff must be integer within the range [0, 255]")
-        self.engine._set_lr_max_diff(lr_max_diff)
+        self.engine.set_lr_max_diff(lr_max_diff)
 
     def _get_registration_mat(self, ir_size, k_ir, k_rgb, ir2rgb):
         R = ir2rgb[:3, :3]
--- python/pysimsense.hpp
+++ python/pysimsense.hpp
@@ -7,35 +7,37 @@
 
 namespace py = pybind11;
 
-using namespace simsense;
-
 void buildSimsense(py::module &parent) {
     py::module m = parent.def_submodule("simsense");
 
-    auto PySimsense = py::class_<DepthSensorEngine>(m, "DepthSensorEngine");
+    auto PySimsense = py::class_<simsense::DepthSensorEngine>(m, "DepthSensorEngine");
 
     PySimsense.def(
         py::init<
-            uint32_t, uint32_t, float, float, float, float, bool,
-            uint8_t, uint8_t, uint32_t, uint8_t, uint8_t, uint8_t, uint8_t,
-            uint8_t, uint8_t, uint8_t, py::array_t<float>, py::array_t<float>,
-            py::array_t<float>, py::array_t<float>
+            uint32_t, uint32_t, float, float, float, float, uint64_t,
+            float, float, float, float, bool, uint8_t, uint8_t, uint32_t,
+            uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t,
+            py::array_t<float>, py::array_t<float>, py::array_t<float>, py::array_t<float>
         >()
     );
     PySimsense.def(
         py::init<
             uint32_t, uint32_t, uint32_t, uint32_t, float, float, float,
-            float, bool, uint8_t, uint8_t, uint32_t, uint8_t, uint8_t,
-            uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, py::array_t<float>,
+            float, uint64_t, float, float, float, float,  bool, uint8_t,
+            uint8_t, uint32_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t,
+            uint8_t, uint8_t, py::array_t<float>, py::array_t<float>,
             py::array_t<float>, py::array_t<float>, py::array_t<float>,
-            py::array_t<float>, py::array_t<float>, py::array_t<float>,
-            float, float, float, bool
+            py::array_t<float>, py::array_t<float>, float, float, float, bool
         >()
     );
-    PySimsense.def("compute", &DepthSensorEngine::compute);
-    PySimsense.def("_set_penalties", &DepthSensorEngine::setPenalties);
-    PySimsense.def("_set_census_window_size", &DepthSensorEngine::setCensusWindowSize);
-    PySimsense.def("_set_matching_block_size", &DepthSensorEngine::setMatchingBlockSize);
-    PySimsense.def("_set_uniqueness_ratio", &DepthSensorEngine::setUniquenessRatio);
-    PySimsense.def("_set_lr_max_diff", &DepthSensorEngine::setLrMaxDiff);
+    PySimsense.def("compute", py::overload_cast<py::capsule, py::capsule>(&simsense::DepthSensorEngine::compute));
+    PySimsense.def("compute", py::overload_cast<py::array_t<uint8_t>, py::array_t<uint8_t>>(&simsense::DepthSensorEngine::compute));
+    PySimsense.def("get_ndarray", &simsense::DepthSensorEngine::getNdarray);
+    PySimsense.def("get_dl_tensor", &simsense::DepthSensorEngine::getDLTensor);
+    PySimsense.def("set_ir_noise_parameters", &simsense::DepthSensorEngine::setInfraredNoiseParameters);
+    PySimsense.def("set_census_window_size", &simsense::DepthSensorEngine::setCensusWindowSize);
+    PySimsense.def("set_matching_block_size", &simsense::DepthSensorEngine::setMatchingBlockSize);
+    PySimsense.def("set_penalties", &simsense::DepthSensorEngine::setPenalties);
+    PySimsense.def("set_uniqueness_ratio", &simsense::DepthSensorEngine::setUniquenessRatio);
+    PySimsense.def("set_lr_max_diff", &simsense::DepthSensorEngine::setLrMaxDiff);
 }
