0b9a103a3726003ea34a235826f21b2a0aa0f4dd
Fanbo Xiang <fxiang@eng.ucsd.edu>
2023-11-15 13:44:25 -0800

    fix imgui.ini

--- python/py_package/utils/viewer/imgui_ini.py
+++ python/py_package/utils/viewer/imgui_ini.py
@@ -1,53 +1,58 @@
-imgui_ini = """ [Window][DockSpace]
-Pos=0,0
-Size=1920,1080
-Collapsed=0
-
-[Window][Control##]
+imgui_ini = """[Window][Control##]
 Pos=0,29
-Size=399,493
+Size=398,526
 Collapsed=0
-DockId=0x00000005,0
+DockId=0x00000004,0
 
 [Window][Scene##]
-Pos=0,524
-Size=399,556
+Pos=0,557
+Size=398,523
 Collapsed=0
-DockId=0x00000006,0
+DockId=0x00000001,0
 
 [Window][Entity##]
 Pos=1520,29
-Size=400,505
+Size=400,526
 Collapsed=0
 DockId=0x00000007,0
 
 [Window][Articulation##]
-Pos=1520,536
-Size=400,544
+Pos=1520,557
+Size=400,523
 Collapsed=0
 DockId=0x00000008,0
 
 [Window][Transform##]
-Pos=0,524
-Size=399,556
+Pos=0,557
+Size=398,523
 Collapsed=0
-DockId=0x00000006,1
+DockId=0x00000001,1
 
 [Window][Render##]
 Pos=0,29
-Size=399,493
+Size=398,526
 Collapsed=0
-DockId=0x00000005,1
+DockId=0x00000004,1
+
+[Window][DockSpace]
+Pos=0,0
+Size=1920,1080
+Collapsed=0
+
+[Window][Debug##Default]
+Pos=60,60
+Size=400,400
+Collapsed=0
 
 [Docking][Data]
-DockSpace       ID=0x4BBE4C7A Window=0x4647B76E Pos=0,29 Size=1920,1051 Split=X
-  DockNode      ID=0x00000003 Parent=0x4BBE4C7A SizeRef=1518,1040 Split=X
-    DockNode    ID=0x00000001 Parent=0x00000003 SizeRef=399,1040 Split=Y Selected=0x9CA6559F
-      DockNode  ID=0x00000005 Parent=0x00000001 SizeRef=399,493 Selected=0x9CA6559F
-      DockNode  ID=0x00000006 Parent=0x00000001 SizeRef=399,556 Selected=0x15CB5A92
-    DockNode    ID=0x00000002 Parent=0x00000003 SizeRef=1519,1040 CentralNode=1
-  DockNode      ID=0x00000004 Parent=0x4BBE4C7A SizeRef=400,1040 Split=Y Selected=0x20E45936
-    DockNode    ID=0x00000007 Parent=0x00000004 SizeRef=400,500 Selected=0x20E45936
-    DockNode    ID=0x00000008 Parent=0x00000004 SizeRef=400,538 Selected=0x24C99AC4
-
+DockSpace       ID=0x4BBE4C7A Pos=0,29 Size=1920,1051 CentralNode=1
+DockSpace       ID=0xFE0324EA Window=0x9A404470 Pos=0,29 Size=1920,1051 Split=X
+  DockNode      ID=0x00000005 Parent=0xFE0324EA SizeRef=1518,1051 Split=X
+    DockNode    ID=0x00000002 Parent=0x00000005 SizeRef=398,1051 Split=Y Selected=0x9CA6559F
+      DockNode  ID=0x00000004 Parent=0x00000002 SizeRef=398,526 Selected=0x9CA6559F
+      DockNode  ID=0x00000001 Parent=0x00000002 SizeRef=398,523 Selected=0x15CB5A92
+    DockNode    ID=0x00000003 Parent=0x00000005 SizeRef=1520,1051 CentralNode=1
+  DockNode      ID=0x00000006 Parent=0xFE0324EA SizeRef=400,1051 Split=Y Selected=0x20E45936
+    DockNode    ID=0x00000007 Parent=0x00000006 SizeRef=400,526 Selected=0x20E45936
+    DockNode    ID=0x00000008 Parent=0x00000006 SizeRef=400,523 Selected=0x24C99AC4
 """
