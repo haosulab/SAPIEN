215c3fc78d6960be50b81f0663a8ff829ae98e45
Fanbo Xiang <fxiang@eng.ucsd.edu>
2024-03-20 14:56:40 -0700

    remove easy profiler completely

--- include/sapien/profiler.h
+++ include/sapien/profiler.h
@@ -14,7 +14,9 @@ public:
 
 }; // namespace sapien
 
-#define SAPIEN_PROFILE_FUNCTION ::sapien::ProfilerBlock spaien_profiler_block_##__LINE__(__func__);
-#define SAPIEN_PROFILE_BLOCK(name) ::sapien::ProfilerBlock spaien_profiler_block_##__LINE__(#name);
+#define SAPIEN_PROFILE_CONCAT_(prefix, suffix) prefix##suffix
+#define SAPIEN_PROFILE_CONCAT(prefix, suffix) SAPIEN_PROFILE_CONCAT_(prefix, suffix)
+#define SAPIEN_PROFILE_FUNCTION ::sapien::ProfilerBlock SAPIEN_PROFILE_CONCAT(sapien_profiler_block_, __LINE__)(__func__);
+#define SAPIEN_PROFILE_BLOCK(name) ::sapien::ProfilerBlock SAPIEN_PROFILE_CONCAT(sapien_profiler_block_,__LINE__)(#name);
 #define SAPIEN_PROFILE_BLOCK_BEGIN(name) ::sapien::ProfilerBlockBegin(#name);
 #define SAPIEN_PROFILE_BLOCK_END ::sapien::ProfilerBlockEnd();
