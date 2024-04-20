da30fa1822ff2f0d6344488ad60b8e35a615798e
Fanbo Xiang <fxiang@eng.ucsd.edu>
2024-04-20 14:33:26 -0700

    fix a unittest

--- unittest/test_scene.py
+++ unittest/test_scene.py
@@ -1,21 +1,23 @@
 import unittest
-import sapien.core as sapien
-from common import *
+import sapien
+from common import rand_pose, rand_size
 import numpy as np
 
 
 class TestScene(unittest.TestCase):
     def test_create_scene(self):
-        engine = sapien.Engine()
-        scene = engine.create_scene()
+        scene = sapien.Scene()
+        self.assertTrue(isinstance(scene.physx_system, sapien.physx.PhysxCpuSystem))
+        self.assertTrue(isinstance(scene.render_system, sapien.render.RenderSystem))
+        scene = sapien.Scene([])
+        with self.assertRaises(RuntimeError):
+            scene.physx_system
+        with self.assertRaises(RuntimeError):
+            scene.render_system
 
     def test_config(self):
-        engine = sapien.Engine()
-        config = sapien.SceneConfig()
+        config = sapien.physx.PhysxSceneConfig()
         config.gravity = [0, 0, -1]
-        config.default_static_friction = 0.2
-        config.default_dynamic_friction = 0.19
-        config.default_restitution = 0.05
         config.bounce_threshold = 1.0
         config.sleep_threshold = 0.001
         config.contact_offset = 0.02
@@ -26,14 +28,12 @@ class TestScene(unittest.TestCase):
         config.enable_ccd = True
         config.enable_enhanced_determinism = True
         config.enable_friction_every_iteration = False
-        config.disable_collision_visual = True
-        scene = engine.create_scene(config)
-        config1 = scene.get_config()
+
+        sapien.physx.set_scene_config(config)
+        scene = sapien.Scene()
+        config = scene.physx_system.get_config()
 
         self.assertAlmostEqual(tuple(config.gravity), (0, 0, -1))
-        self.assertAlmostEqual(config.default_static_friction, 0.2)
-        self.assertAlmostEqual(config.default_dynamic_friction, 0.19)
-        self.assertAlmostEqual(config.default_restitution, 0.05)
         self.assertAlmostEqual(config.bounce_threshold, 1.0)
         self.assertAlmostEqual(config.sleep_threshold, 0.001)
         self.assertAlmostEqual(config.contact_offset, 0.02)
@@ -44,16 +44,18 @@ class TestScene(unittest.TestCase):
         self.assertEqual(config.enable_ccd, True)
         self.assertEqual(config.enable_enhanced_determinism, True)
         self.assertEqual(config.enable_friction_every_iteration, False)
-        self.assertEqual(config.disable_collision_visual, True)
+
+        # self.assertAlmostEqual(config.default_static_friction, 0.2)
+        # self.assertAlmostEqual(config.default_dynamic_friction, 0.19)
+        # self.assertAlmostEqual(config.default_restitution, 0.05)
 
     def test_actor_builder(self):
-        engine = sapien.Engine()
-        scene = engine.create_scene()
+        scene = sapien.Scene()
         builder = scene.create_actor_builder()
 
         poses = [rand_pose() for _ in range(10)]
         sizes = [rand_size() for _ in range(10)]
-        mats = [engine.create_physical_material(*np.random.rand(3)) for _ in range(10)]
+        mats = [sapien.physx.PhysxMaterial(*np.random.rand(3)) for _ in range(10)]
         densities = [np.random.rand() * 1000 for _ in range(10)]
         patch_radii = [np.random.rand() for _ in range(10)]
         min_patch_radii = [np.random.rand() for _ in range(10)]
@@ -93,7 +95,7 @@ class TestScene(unittest.TestCase):
         )
         idx += 1
 
-        builder.add_collision_from_file(
+        builder.add_convex_collision_from_file(
             filename="assets/cone.stl",
             pose=poses[idx],
             scale=sizes[idx],
@@ -115,20 +117,20 @@ class TestScene(unittest.TestCase):
             is_trigger=False,
         )
 
-        collisions = builder.get_collisions()
+        collisions = builder.collision_records
         self.assertEqual(len(collisions), 5)
         for i, c in enumerate(collisions):
             self.assertTrue(np.allclose(c.pose.p, poses[i].p))
             self.assertTrue(np.allclose(c.pose.q, poses[i].q))
             self.assertEqual(c.material, mats[i])
-            if c.type != "Nonconvex":
+            if c.type != "nonconvex_mesh":
                 self.assertTrue(np.allclose(c.density, densities[i]))
 
-        self.assertEqual(collisions[0].type, "Box")
-        self.assertEqual(collisions[1].type, "Sphere")
-        self.assertEqual(collisions[2].type, "Capsule")
-        self.assertEqual(collisions[3].type, "Mesh")
-        self.assertEqual(collisions[4].type, "Nonconvex")
+        self.assertEqual(collisions[0].type, "box")
+        self.assertEqual(collisions[1].type, "sphere")
+        self.assertEqual(collisions[2].type, "capsule")
+        self.assertEqual(collisions[3].type, "convex_mesh")
+        self.assertEqual(collisions[4].type, "nonconvex_mesh")
 
         self.assertTrue(np.allclose(collisions[0].scale, sizes[0]))
         self.assertTrue(np.allclose(collisions[1].radius, sizes[1][0]))
@@ -137,26 +139,50 @@ class TestScene(unittest.TestCase):
         self.assertTrue(np.allclose(collisions[3].scale, sizes[3]))
         self.assertTrue(np.allclose(collisions[4].scale, sizes[4]))
 
-        actor = builder.build_kinematic()
-        self.assertEqual(actor.type, "kinematic")
+        body = builder.build_kinematic().find_component_by_type(
+            sapien.physx.PhysxRigidBaseComponent
+        )
+        self.assertTrue(body.kinematic)
         self.assertEqual(
-            tuple(c.type for c in actor.get_collision_shapes()),
-            ("box", "sphere", "capsule", "convex_mesh", "nonconvex_mesh"),
+            tuple(c.__class__ for c in body.get_collision_shapes()),
+            (
+                sapien.physx.PhysxCollisionShapeBox,
+                sapien.physx.PhysxCollisionShapeSphere,
+                sapien.physx.PhysxCollisionShapeCapsule,
+                sapien.physx.PhysxCollisionShapeConvexMesh,
+                sapien.physx.PhysxCollisionShapeTriangleMesh,
+            ),
         )
 
-        actor = builder.build_static()
-        self.assertEqual(actor.type, "static")
+        body = builder.build_static().find_component_by_type(
+            sapien.physx.PhysxRigidBaseComponent
+        )
+        self.assertTrue(isinstance(body, sapien.physx.PhysxRigidStaticComponent))
         self.assertEqual(
-            tuple(c.type for c in actor.get_collision_shapes()),
-            ("box", "sphere", "capsule", "convex_mesh", "nonconvex_mesh"),
+            tuple(c.__class__ for c in body.get_collision_shapes()),
+            (
+                sapien.physx.PhysxCollisionShapeBox,
+                sapien.physx.PhysxCollisionShapeSphere,
+                sapien.physx.PhysxCollisionShapeCapsule,
+                sapien.physx.PhysxCollisionShapeConvexMesh,
+                sapien.physx.PhysxCollisionShapeTriangleMesh,
+            ),
         )
 
-        builder.remove_collision_at(4)
-        actor = builder.build()
-        self.assertEqual(actor.type, "dynamic")
+        del builder.collision_records[4]
+        builder.physx_body_type = "dynamic"
+        body = builder.build().find_component_by_type(
+            sapien.physx.PhysxRigidBaseComponent
+        )
+        self.assertFalse(body.kinematic)
         self.assertEqual(
-            tuple(c.type for c in actor.get_collision_shapes()),
-            ("box", "sphere", "capsule", "convex_mesh"),
+            tuple(c.__class__ for c in body.get_collision_shapes()),
+            (
+                sapien.physx.PhysxCollisionShapeBox,
+                sapien.physx.PhysxCollisionShapeSphere,
+                sapien.physx.PhysxCollisionShapeCapsule,
+                sapien.physx.PhysxCollisionShapeConvexMesh,
+            ),
         )
 
         # TODO: check details of the built shapes
