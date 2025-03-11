2b6499f531d12729ab9903dc9861a863f7fb9809
Fanbo Xiang <xiangfanbo@gmail.com>
2025-03-11 11:03:40 -0700

    try fixing build scripts

--- cmake/pybind11.cmake
+++ cmake/pybind11.cmake
@@ -6,9 +6,7 @@ include(FetchContent)
 FetchContent_Declare(
   pybind11
   GIT_REPOSITORY https://github.com/pybind/pybind11.git
-  GIT_TAG 2943a27a14b507c67ca3e17c57bb74bbb7744f2b
-  GIT_SHALLOW TRUE
-  GIT_PROGRESS TRUE
+  GIT_TAG 06e8ee2e357fc2fd6e36de431fa0ca0049aafc7d
 )
 
 FetchContent_MakeAvailable(pybind11)
